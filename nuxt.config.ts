84d8681f56dec86978f14e0efdc78abaf8e41219
Hanh20021706 <hanhs8155@gmail.com>
2024-04-09 14:32:28 +0700

    feat: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  css: ["@/assets/css/index.css"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2 class="text-red-500">hello world</h2>
+  </div>
+</template>
