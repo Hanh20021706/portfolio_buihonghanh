e69f7d1368f0521fec5489ac8012328582efae57
Hanh20021706 <hanhs8155@gmail.com>
2024-06-07 15:20:25 +0700

    feat: ui section about me

---
+++ components/About-me.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container pt-122">
+    <BaseTitleSection :title="$t('aboutMe.title')"></BaseTitleSection>
+
+    <div class="">
+      <p class="text-white pb-[28px] text-[16px] leading-loose">
+        {{ $t("aboutMe.desc") }}
+      </p>
+      <NuxtLink
+        to="/"
+        class="text-white border border-white py-[6px] px-[24px]"
+      >
+        {{ $t("aboutMe.buttonLink") }} ~>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
