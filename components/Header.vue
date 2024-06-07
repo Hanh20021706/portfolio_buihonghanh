1c4642b29f0cfe258216bb7933a2873eda5c33a0
Hanh20021706 <hanhs8155@gmail.com>
2024-06-07 17:32:54 +0700

    config ui

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,62 @@
 <template>
-  <div class="bg-primaryLight py-[24px]">
+  <div class="bg-primaryLight py-[24px] sticky top-0 left-0 right-0 z-50">
     <div class="container">
-      <div class="flex justify-between">
+      <!-- header desktop -->
+      <div class="md:flex justify-between hidden">
         <ul class="flex gap-[24px]">
+          <li>
+            <NuxtLink
+              to="/"
+              class="text-neutralWhiteDark"
+              active-class="text-white"
+            >
+              {{ $t("header.home") }}
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              to="/projects"
+              class="text-neutralWhiteDark"
+              active-class="text-white"
+            >
+              {{ $t("header.projects") }}
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              to="/about"
+              class="text-neutralWhiteDark"
+              active-class="text-white"
+            >
+              {{ $t("header.aboutMe") }}
+            </NuxtLink>
+          </li>
+        </ul>
+        <div class="flex items-center gap-[24px]">
+          <Language></Language>
+          <SvgoIconLogo class="!w-[26px] !h-[26px] !text-white" />
+        </div>
+      </div>
+      <!-- header mobile -->
+      <div class="flex bg-primaryLight md:hidden justify-between">
+        <div class="">
+          <NuxtLink to="/">
+            <SvgoIconLogo class="!w-[26px] !h-[26px] !text-white" />
+          </NuxtLink>
+        </div>
+        <div class="cursor-pointer" @click="isShowBar = !isShowBar">
+          <SvgoIconBar
+            class="!w-[26px] !h-[26px] !text-white"
+            v-if="!isShowBar"
+          />
+          <SvgoIconClose
+            class="!w-[26px] !h-[26px] !text-white"
+            v-if="isShowBar"
+          />
+        </div>
+      </div>
+      <div v-if="isShowBar" class="bg-primaryLight mt-[36px] h-[100vh]">
+        <ul class="flex flex-col gap-[24px]">
           <li>
             <NuxtLink to="/" class="text-white">
               {{ $t("header.home") }}
@@ -18,11 +72,10 @@
               {{ $t("header.aboutMe") }}
             </NuxtLink>
           </li>
+          <li>
+            <Language></Language>
+          </li>
         </ul>
-        <div class="flex items-center gap-[24px]">
-          <Language></Language>
-          <SvgoIconLogo class="!w-[26px] !h-[26px] !text-white" />
-        </div>
       </div>
     </div>
   </div>
@@ -35,6 +88,12 @@ export default {
   components: {
     Language,
   },
+  data() {
+    return {
+      isShowBar: false,
+    };
+  },
+  methods: {},
 };
 </script>
 
--- locales/vn.js
+++ locales/vn.js
@@ -6,4 +6,13 @@ export default {
     vietnamese: "VN",
     english: "EN",
   },
+  banner: {
+    name: "Bùi Hồng Hạnh",
+    desc: "Tôi là nhà thiết kế website và nhà phát triển giao diện người dùng",
+  },
+  aboutMe: {
+    title: "Về tôi",
+    desc: "Tôi bắt đầu công việc của mình với vai trò là Front-end Developer, tham gia các dự án thực tế và phức tạp như ERP, POS... Từ khi tốt nghiệp tôi đã có kiến ​​thức về thiết kế kết hợp với kiến ​​thức về thiết kế. Kiến thức lập trình đã giúp tôi phát triển và trở thành Nhà thiết kế website bằng việc luôn đặt trải nghiệm người dùng (UX) và giao diện người dùng (UI) lên hàng đầu.",
+    buttonLink: "Xem thêm",
+  },
 };
